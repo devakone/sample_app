3250303bcbde0e8f5f687b514bcc7c98550bf048
Abou Kone <adkone@gmail.com>
2012-02-17 12:43:18 -0500

    Added the @base_title instance variable to the PageController

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,12 @@
   
   describe PagesController do
     render_views
+    
+    before(:each) do
+      @base_title= "Ruby on Rails Tutorial Sample App"
+    end
+
+    
     describe "GET 'home'" do
       it "should be successful" do
         get 'home'
@@ -11,7 +17,7 @@
       it "should have the right title" do
         get 'home'
         response.should have_selector("title",
-                                      :content => "Ruby on Rails Tutorial Sample App | Home")
+                                      :content => @base_title + " | Home")
       end
     end
   
@@ -23,7 +29,7 @@
       it "should have the right title" do
         get 'contact'
         response.should have_selector("title",
-                                      :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                      :content => @base_title + " | Contact")
       end
     end
     
@@ -35,7 +41,7 @@
       it "should have the right title" do
         get 'about'
         response.should have_selector("title",
-                                      :content => "Ruby on Rails Tutorial Sample App | About")
+                                      :content => @base_title + " | About")
       end
      end
      
@@ -47,7 +53,7 @@
       it "should have the right title" do
         get 'help'
         response.should have_selector("title",
-                                      :content => "Ruby on Rails Tutorial Sample App | Help")
+                                      :content => @base_title + " | Help")
       end
     end
   
